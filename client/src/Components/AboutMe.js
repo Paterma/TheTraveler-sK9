a71dd5a1e6a38e25cf71604846ec23f0f26c14ed
Maria Paterno <mariapaterno7@yahoo.com>
2022-07-09 19:23:15 -0600

    added description, adjusted about me page

--- client/src/Components/AboutMe.js
+++ client/src/Components/AboutMe.js
@@ -6,18 +6,39 @@ import { Carousel } from 'react-responsive-carousel';
 import review1 from '../Styles/review1.png'
 import review2 from '../Styles/review2.png'
 import review3 from '../Styles/review3.png'
+import Airbus from '../Styles/photo2.PNG'
 
 
 
 function AboutMe() {
 
 return (
-    
+    <div class="container">
+<div class="row align-items-start">
+    <div class="col" id="comments">
+    "She is amazing" -Me
+    </div>
+    <div class="col">
+    <p id="introParagraph">Welcome to my site, I'm Chloe, and I created The Traveler's K9 in 2022 after working professionally with dogs (and other animals) for 6 years. I'm a balanced dog trainer who focuses on all quadrants of operant conditioning. You may be wondering where "Traveler's" come's from- I run a mobile business that allows me to be in remote locations; having a broad array of clients. My passion's include dog sports, thru hikes, and of course traveling with my dog. My mission it to create a profound bond between human and dog, resulting in a well mannered environment. </p>
+<img src={Chloe} className="img-fluid" id="chloeAndAirbus" alt="Chloe and Airbus"/>
+    </div>
+    <div class="col" id="trainingPics">
+    <img src={Airbus} id="training" />
+    </div>
+</div>
 <div>
+</div>
+</div>
+)
+}
+
+export default AboutMe;
+
+{/* <div>
 <p id="introParagraph">Welcome to The Traveler's K9! My name is Chloe Pfahl and I have been a professional dog trainer for 6 years now. </p>
 <img src={Chloe} className="img-fluid" id="chloeAndAirbus" alt="Chloe and Airbus"/>
 <div id="testimonials">
-<Carousel autoPlay="true" infiniteLoop="true" showIndicators="false" showArrows="false" transitionTime={8} centerMode="true">
+<Carousel autoPlay="true" infiniteLoop="true" showIndicators="false" showArrows="false" transitionTime={5} centerMode="true">
         <div>
                     <img src={review1} />
                 </div>
@@ -29,8 +50,4 @@ return (
                 </div>
     </Carousel>
 </div>
-</div>
-)
-}
-
-export default AboutMe;
+</div> */}
--- client/src/Components/Services.js
+++ client/src/Components/Services.js
@@ -6,48 +6,90 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function Services() {
 
     return (
-        <div id="servicePage">
-    <div id="serviceList">
-        <h2 id="serviceHead">Services</h2>
-        <ol class="list-group list-group-numbered" id="serviceList">
-  <li class="list-group-item d-flex justify-content-between align-items-start" id="serviceList">
-    <div class="ms-2 me-auto" id="serviceList">
-      <div class="fw-bold" id="serviceList">Puppy Training</div>
-      Description
+<div class="accordion" id="accordionExample">
+  <p id="trainingDesc">Training is contigent upon your specific dog, and their particular needs. These packages are outlines to be customized during evaluation.</p>
+  <p id="evalutions">Evaluations are $65 for 1 hour and required before package purchase or board-and-train.</p>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Puppy Training (must be under 6 months old)
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      Puppy socialization, crate training, teething/nipping, potty training, intro to sit, down, place, and loose leash walking. <br></br> <strong> 5 lessons for <span>$500</span>, 7 lessons for <span>$700</span> (includes 2 public outings)</strong>
+      </div>
     </div>
-    <span class="badge bg-success rounded-pill">$120</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start" id="serviceList">
-    <div class="ms-2 me-auto" id="serviceList">
-      <div class="fw-bold" id="serviceList">Dog Training</div>
-      Description
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Adult Dog Training
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+        Loose leash walking/heeling, sit, down, place, stay, and recall. <br></br> <strong> 5 lessons for <span>$625</span>, 7 lessons for <span>$875</span></strong>
+      </div>
     </div>
-    <span class="badge bg-dark rounded-pill" >$130</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start" id="serviceList">
-    <div class="ms-2 me-auto" id="serviceList">
-      <div class="fw-bold" id="serviceList">Board and Train</div>
-      Description
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Advanced Training (after adult dog training)
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      E-Collar training for off-leash capability, the three D's (distance, duration, and distraction) for heeling, recall, place, sit, and down-stay.
+      <br></br>
+      <strong> 5 lessons for <span>$675</span>, 7 lessons for <span>$945</span> (includes 2 public outings)</strong>
+      </div>
     </div>
-    <span class="badge bg-secondary rounded-pill">$180</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start" id="serviceList">
-    <div class="ms-2 me-auto" id="serviceList">
-      <div class="fw-bold" id="serviceList">Board Only</div>
-      Description
+  </div>
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+      Board and Train
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+Board and train catered to your dog's personal needs. E-collar training is a minimum two week board and train. Maximum 2 dogs at a time to ensure a comfortable environment for all dogs, within my personal home.  Duration of board-and-train determined at eval.
+      <br></br>
+      <strong><span>$150</span> a day, <span>$1050</span> a week, <span>$2100</span> for a two week board-and-train</strong>
+      </div>
     </div>
-    <span class="badge bg-dark rounded-pill" >$180</span>
-  </li>
-  <li class="list-group-item d-flex justify-content-between align-items-start" id="serviceList">
-    <div class="ms-2 me-auto" id="serviceList">
-      <div class="fw-bold" id="serviceList">Package Deals</div>
-      Description
+  </div>
+
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Board Only
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      House manners practiced, exercised daily, and play time with social dogs allowed. Maximum 2 dogs at a time within my personal home. <strong id="red">Will not take dogs who are deemed unsafe in a crate.</strong> Contact for more details.
+      <br></br>
+      <strong><span>$100</span> a day, <span> $80</span> a day if they have completed a board-and-train within the past 6 months</strong>
+      </div>
     </div>
-    <span class="badge bg-dark rounded-pill" >$600</span>
-  </li>
-</ol>
+  </div>
+  
+  <div class="accordion-item">
+    <h2 class="accordion-header" id="headingOne">
+      <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+        Group Lessons
+      </button>
+    </h2>
+    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+      <div class="accordion-body">
+      <strong id="soon">Coming soon!</strong>
+      </div>
     </div>
-    </div>
+  </div>
+</div>
     )
     }
     
