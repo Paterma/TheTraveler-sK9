facc404183e427502f26806a3f6a4279bad2aaf8
Maria Paterno <mariapaterno7@yahoo.com>
2022-07-23 13:05:06 -0600

    server built

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const app = express()
+
+const path = require('path')
+
+const PORT = process.env.PORT || 3000
+
+app.use(express.static(path.join(__dirname + "/public")))
+
+app.listen(PORT)
