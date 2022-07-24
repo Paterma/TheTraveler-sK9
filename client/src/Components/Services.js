9f0c7409b0bdf93a459a6e84ed31571b3230cfb7
Maria Paterno <mariapaterno7@yahoo.com>
2022-07-24 14:07:18 -0600

    updated accordian

--- client/src/Components/Services.js
+++ client/src/Components/Services.js
@@ -15,18 +15,19 @@ function Services() {
 
               <p id="trainingDesc">Training is contigent upon your specific dog, and their particular needs. These packages are outlines to be customized during evaluation.  </p>
               <p id="evalutions">Evaluations are $65 for 1 hour and required before package purchase or board-and-train.</p>
-<Accordion> <img className="img-fluid mx-auto d-block" id="fullLogo" src={logoFull} alt="The Traveler's K0 Logo" />
+<Accordion> <img className="img-fluid mx-auto d-block" id="fullLogo" src={logoFull} alt="The Traveler's K9 Logo" />
       <Accordion.Item eventKey="0">
-        <Accordion.Header>Puppy Package- For 6 month old dogs or YOUNGER</Accordion.Header>
+        <Accordion.Header><strong>Puppy Package- &ensp; </strong>For 6 month old dogs or YOUNGER</Accordion.Header>
+
         <Accordion.Body>
         <p>Loose Leash Walking/Heeling, Sit, Down, Stay, Place, Recall, Boundaries</p>
-        <strong> 5 lessons (in-home) <span>$625</span>, 7 lessons (in-home plus two public outings) <span>$875</span></strong>
+        <strong> 5 lessons (in-home) <span>$500</span>, 7 lessons (in-home plus two public outings) <span>$700</span></strong>
         </Accordion.Body>
       </Accordion.Item>
 
 
       <Accordion.Item eventKey="1">
-        <Accordion.Header>Adult Dog Package- For dogs older than 6 months</Accordion.Header>
+        <Accordion.Header><strong>Adult Dog Package- &ensp;</strong>   For dogs older than 6 months</Accordion.Header>
         <Accordion.Body>
         <p>Adding Distance, Duration, and Distraction to known behaviors such as Recalls, Stays, Heeling, and Place.</p>
         <strong> 5 lessons <span>$625</span>, 7 lessons (in-home plus two public outings) <span>$875</span></strong>
@@ -34,7 +35,7 @@ function Services() {
       </Accordion.Item>
 
       <Accordion.Item eventKey="2">
-        <Accordion.Header>Advanced Dog Package with E-Collar Training-  For dogs who have completed Adult Dog Package
+        <Accordion.Header> <strong>Advanced Dog Package with E-Collar Training- &ensp;</strong> For dogs who have completed Adult Dog Package
         content</Accordion.Header>
         <Accordion.Body>
         <p>E-Collar conditioning for Off-Leash Reliability. Adding Distance, Duration, and Distraction to known behaviors such as Recalls, Stays, Heeling, and Place.</p>
@@ -43,7 +44,7 @@ function Services() {
       </Accordion.Item>
 
       <Accordion.Item eventKey="3">
-        <Accordion.Header>Private Lesson</Accordion.Header>
+        <Accordion.Header><strong>Private Lesson</strong></Accordion.Header>
         <Accordion.Body>
         <p>Hour long session at your residency or public dog friendly location. Will work on any desired behavior in the time allocated. Used primarily for behavioral cases.</p>
         <strong><span>$140</span></strong>
@@ -51,7 +52,7 @@ function Services() {
       </Accordion.Item>
 
       <Accordion.Item eventKey="4">
-        <Accordion.Header>Board and Train- Minimum 1 week for all dogs. Length of stay determined at Evaluation</Accordion.Header>
+        <Accordion.Header> <strong>Board and Train- &ensp;</strong> Minimum 1 week for all dogs. Length of stay determined at Evaluation</Accordion.Header>
         <Accordion.Body>
         <p>Board and Train goals established during evaluation. E-Collar Training requires a MINIMUM of a two week stay. Dog's will be living with Trainer with same care and training given to their own personal dogs. Exercise and training requirements will be met during Board and Train. Seperate Board and Train contract available and will need to be signed prior to boarding arrangements.</p>
       <strong> 1 Week <span>$1050 </span>($150/day),   2 Weeks <span>$2100 </span>($150/day),   3 Weeks+ <span>$2940 </span>($140/day)</strong>
@@ -59,7 +60,7 @@ function Services() {
       </Accordion.Item>
 
       <Accordion.Item eventKey="5">
-        <Accordion.Header>Boarding Only- Only offered for qualified clients. Contact for details and qualification</Accordion.Header>
+        <Accordion.Header> <strong>Boarding Only- &ensp;</strong> Only offered for qualified clients. Contact for details and qualification</Accordion.Header>
         <Accordion.Body>
         <p><strong>MUST BE CRATE TRAINED.</strong></p>
        <p>Boarding dogs will practice known behaviors and proper boundaries within the home. They will be exercised and given mental stimulation daily.</p>
@@ -68,7 +69,7 @@ function Services() {
       </Accordion.Item>
 
       <Accordion.Item eventKey="6">
-        <Accordion.Header>Group Lessons- Only offered for qualified clients</Accordion.Header>
+        <Accordion.Header> <strong>Group Lessons- &ensp;</strong> Only offered for qualified clients</Accordion.Header>
         <Accordion.Body>
         Qualified Clients include those who have completed minimum evaluation and 3 in-home private lessons. Qualified Clients are determined based on Trainer's evaluation of dog's current skillset and benefit of being in a group setting.
           <p>Group Lesson determined in advance with a 5 member maximum. Location Varies. Group sessions minimum 2 hour duration.</p>
