2f6550a5a7c3174c519cf2d1a400be7397d4b795
Maria Paterno <mariapaterno7@yahoo.com>
2022-07-24 12:43:13 -0600

    added photos

--- client/src/Components/Photos.js
+++ client/src/Components/Photos.js
@@ -5,7 +5,14 @@ import { Carousel } from 'react-responsive-carousel';
 import photo1 from '../Styles/photo1.PNG'
 import photo2 from '../Styles/photo2.PNG'
 import photo3 from '../Styles/photo4.jpg'
-
+import photo4 from '../Styles/barnhunt.PNG'
+import photo5 from '../Styles/4pups.PNG'
+import photo6 from '../Styles/magook.PNG'
+import photo7 from '../Styles/epups.PNG'
+import photo8 from '../Styles/photo5.PNG'
+import photo9 from '../Styles/photo6.PNG'
+import photo10 from '../Styles/photo7.PNG'
+import photo11 from '../Styles/up.PNG'
 
 function Photos() {
 
@@ -16,7 +23,7 @@ function Photos() {
         <Carousel autoPlay="true" infiniteLoop="true">
         <div>
                     <img src={photo1} />
-                    <p className="legend">My main squeeze, Erebus</p>
+                    <p className="legend">My main squeeze</p>
                 </div>
                 <div>
                     <img src={photo2} />
@@ -26,6 +33,39 @@ function Photos() {
                     <img src={photo3} />
                     <p className="legend">Do you even climb?</p>
                 </div>
+                <div>
+                    <img src={photo4} />
+                    <p className="legend">Rat problem? Call Erebus</p>
+                </div>
+                <div>
+                    <img src={photo6} />
+                    <p className="legend">❤️ Maguyuk ❤️ </p>
+                </div>
+                <div>
+                    <img src={photo5} />
+                    <p className="legend">There's always one who isn't looking... </p>
+                </div>
+                <div>
+                    <img src={photo11} />
+                    <p className="legend">Look pretty!</p>
+                </div>
+                <div>
+                    <img src={photo7} />
+                    <p className="legend">My lip doesn't always get stuck, but when it does... </p>
+                </div>
+                <div>
+                    <img src={photo8} />
+                    <p className="legend">Thank goodness for blankets</p>
+                </div>
+                <div>
+                    <img src={photo9} />
+                    <p className="legend">Happy pups</p>
+                </div>
+                <div>
+                    <img src={photo10} />
+                    <p className="legend">The beginning of our journey on the Colorado Trail!</p>
+                </div>
+             
     </Carousel>
     </div>
     )
